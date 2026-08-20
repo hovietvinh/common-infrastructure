9b5ab3be3a4e2c64fe481eb5d73347559e997d05
Ho Viet Vinh <hovietvinh29032004@gmail.com>
2026-08-20 13:50:02 +0700

    update format

--- terraform/main/modules/eks/helm.tf
+++ terraform/main/modules/eks/helm.tf
@@ -6,20 +6,20 @@ resource "helm_release" "cluster_autoscaler" {
 
   version = "9.59.0"
 
-  set = [
-    {
-      name  = "rbac.serviceAccount.name"
-      value = "cluster-autoscaler"
-    },
-    {
-      name  = "autoDiscovery.clusterName"
-      value = aws_eks_cluster.eks_cluster.name
-    },
-    {
-      name  = "awsRegion"
-      value = "ap-southeast-1"
-    }
-  ]
+  set {
+    name  = "rbac.serviceAccount.name"
+    value = "cluster-autoscaler"
+  }
+
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = aws_eks_cluster.eks_cluster.name
+  }
+
+  set {
+    name  = "awsRegion"
+    value = "ap-southeast-1"
+  }
 }
 
 resource "helm_release" "aws_lbc" {
@@ -30,16 +30,15 @@ resource "helm_release" "aws_lbc" {
   namespace  = "kube-system"
   version    = "1.7.2"
 
-  set = [
-    {
-      name  = "clusterName"
-      value = aws_eks_cluster.eks_cluster.name
-    },
-    {
-      name  = "serviceAccount.name"
-      value = "aws-load-balancer-controller"
-    }
-  ]
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks_cluster.name
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 
   depends_on = [
     helm_release.cluster_autoscaler
@@ -53,37 +52,39 @@ resource "helm_release" "external_dns" {
   namespace  = "kube-system"
   version    = "1.15.0"
 
-  set = [
-    {
-      name  = "provider"
-      value = "aws"
-    },
-    {
-      name  = "aws.region"
-      value = var.region
-    },
-    {
-      name  = "domainFilters[0]"
-      value = var.root_domain
-    },
-    {
-      name  = "rbac.serviceAccount.name"
-      value = "external-dns"
-    },
-    {
-      name  = "policy"
-      value = "sync"
-    }
-  ]
+  set {
+    name  = "provider"
+    value = "aws"
+  }
+
+  set {
+    name  = "aws.region"
+    value = var.region
+  }
+
+  set {
+    name  = "domainFilters[0]"
+    value = var.root_domain
+  }
+
+  set {
+    name  = "rbac.serviceAccount.name"
+    value = "external-dns"
+  }
+
+  set {
+    name  = "policy"
+    value = "sync"
+  }
 }
 
 resource "helm_release" "argocd" {
-  name              = "argocd"
-  repository        = "https://argoproj.github.io/argo-helm"
-  chart             = "argo-cd"
-  namespace         = "argocd"
-  create_namespace  = true
-  version           = "10.3.3"
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "10.3.3"
   values = [
     templatefile("${path.module}/values/argocd.yaml.tpl", {
       argocd_hostname = var.argocd_hostname
