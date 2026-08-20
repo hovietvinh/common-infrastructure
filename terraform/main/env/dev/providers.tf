796976f2642bb3e65f28325f9c4bfcad37e12ba3
Ho Viet Vinh <hovietvinh29032004@gmail.com>
2026-08-20 13:47:28 +0700

    add output eks

--- terraform/main/env/dev/providers.tf
+++ terraform/main/env/dev/providers.tf
@@ -6,9 +6,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.14"
+    }
   }
 }
-
 
 provider "aws" {
   region = var.region
@@ -18,7 +25,25 @@ provider "aws" {
       Environment = var.env
       Project     = var.project_name
       ManagedBy   = "Terraform"
-      Type = "Main"
+      Type        = "Main"
     }
+  }
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
---
+++ terraform/main/modules/eks/output.tf
@@ -0,0 +1,11 @@
+output "cluster_name" {
+  value       = aws_eks_cluster.eks_cluster.name
+}
+
+output "cluster_ca" {
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "cluster_endpoint" {
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
